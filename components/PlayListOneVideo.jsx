7d14b85e783d4f99080dd1c7f571631e424791ec
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2023-02-16 01:32:20 +0900

    [REFACTOR]map 활용하는 부분 수정

--- components/PlayListOneVideo.jsx
+++ components/PlayListOneVideo.jsx
@@ -3,53 +3,50 @@ import Link from 'next/link';
 import { BsFillPlayFill } from 'react-icons/bs';
 
 function OneVideo({ dataName, youtubePlayList }) {
+  console.log(dataName);
+  console.log(youtubePlayList);
   return (
     <div>
       <ul>
-        {dataName.items.map((item) => {
-          const { id, snippet = {} } = item;
-          const { title, resourceId, publishedAt, channelTitle } = snippet; //destructure 해두는 과정임
-          const { profilepic, name } = youtubePlayList;
-          return (
-            <li key={id}>
-              <div className="flex w-full h-19 items-center border-b-[1px] border-gray-300 border-r-0">
-                <div className="bg-[#252450] h-[76px] w-1.5"></div>
-                <div className="px-1 py-auto">
-                  <Image
-                    src={profilepic}
-                    width="50"
-                    height="50"
-                    objectFit="cover"
-                    alt="/"
-                    className="rounded-full"
-                  />
+        <li>
+          <div className="flex w-full h-19 items-center border-b-[1px] border-gray-300 border-r-0">
+            <div className="bg-[#252450] h-[76px] w-1.5"></div>
+            <div className="px-1 py-auto">
+              <Image
+                src={youtubePlayList.profilepic}
+                width="50"
+                height="50"
+                objectFit="cover"
+                alt="/"
+                className="rounded-full"
+              />
+            </div>
+            <div className="h-[76px] w-full border-r-[1px] border-gray-300">
+              <div className="flex flex-col space-y-0.5 pr-3 justify-center h-full">
+                <h3 className="overflow-hidden text-ellipsis whitespace-nowrap w-[300px] pl-2">
+                  {dataName.items[0].snippet.title}
+                </h3>
+                <div className="flex justify-between pl-2">
+                  <h3 className="text-sm text-[#888888] underline">
+                    {youtubePlayList.name}
+                  </h3>
+                  <h3 className="font-light text-xs">
+                    {new Date(
+                      dataName.items[0].snippet.publishedAt
+                    ).toLocaleDateString()}
+                  </h3>
                 </div>
-                <div className="h-[76px] w-full border-r-[1px] border-gray-300">
-                  <div className="flex flex-col space-y-0.5 pr-3 justify-center h-full">
-                    <h3 className="overflow-hidden text-ellipsis whitespace-nowrap w-[300px] pl-2">
-                      {title}
-                    </h3>
-                    <div className="flex justify-between pl-2">
-                      <h3 className="text-sm text-[#888888] underline">
-                        {name}
-                      </h3>
-                      <h3 className="font-light text-xs">
-                        {new Date(publishedAt).toLocaleDateString()}
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <Link
-                  href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
-                >
-                  <div className="flex w-[50px] border-l-gray-300 justify-center items-center text-3xl text-[#252450] mx-auto h-full cursor-pointer mx-2 ">
-                    <BsFillPlayFill />
-                  </div>
-                </Link>
               </div>
-            </li>
-          );
-        })}
+            </div>
+            <Link
+              href={`https://www.youtube.com/watch?v=${dataName.items[0].id.videoId}`}
+            >
+              <div className="flex w-[50px] border-l-gray-300 justify-center items-center text-3xl text-[#252450] mx-auto h-full cursor-pointer mx-2 ">
+                <BsFillPlayFill />
+              </div>
+            </Link>
+          </div>
+        </li>
       </ul>
     </div>
   );
